bb6b427e83d2bb8bffb3c44b775bceff9fdbc0ca
401asfg <mb.allan@ymail.com>
2023-05-11 11:46:40 -0700

    Duplicate of gotten elem

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,7 +5,8 @@ export default function About() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1>Abou Pages</h1>
+        <h1>About Page</h1>
+        <h1>About Page</h1>
         <p className={styles.description}>
           <Link href="/">&larr; Go Back</Link>
         </p>
